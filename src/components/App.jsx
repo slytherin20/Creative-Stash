1b3f5f2c084bc43e4906aca40ac6e8c510902206
slytherin20 <sonaligupta2904@gmail.com>
2022-01-14 23:16:10 +0530

    FEAT - Login form added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <main className="sans-serif">
+    <main className="sans-serif overflow-hidden">
       <Navbar />
       <MainPage />
       <Routes>
