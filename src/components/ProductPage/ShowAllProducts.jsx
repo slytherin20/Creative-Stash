fb7257399b5439ed780775c2d17feb41e2399dae
slytherin20 <sonaligupta2904@gmail.com>
2022-03-01 14:40:17 +0530

    FEAT - Added show all products page

--- src/components/ProductPage/ShowAllProducts.jsx
+++ src/components/ProductPage/ShowAllProducts.jsx
@@ -1,5 +1,51 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NotFound from "../NotFound.jsx";
+import Item from "../Home Page/Item.jsx";
+import { Link } from "react-router-dom";
 function ShowAllProducts() {
-  return <h1>Show All Products Page.</h1>;
+  const [products, setProducts] = useState({});
+  let params = useParams();
+  let categories = [
+    "Paints",
+    "Painting-Medium",
+    "Canvas",
+    "Brushes",
+    "Pens-and-Markers",
+  ];
+
+  useEffect(() => {
+    if (categories.includes(params.id)) getData();
+  }, []);
+
+  async function getData() {
+    let res = await fetch("http://localhost:3000/Products");
+    let data = await res.json();
+    setProducts(data[params.id.split("-").join(" ")]);
+  }
+
+  function renderCategory() {
+    let arr = [];
+    for (let category in products) {
+      arr.push(
+        <div key={category}>
+          <Item items={products[category].slice(0, 5)} title={category} />
+          {products[category].length > 5 ? (
+            <Link
+              to={`/Products/${params.id}/${category.split(" ").join("-")}`}
+            >
+              <button>Show More</button>
+            </Link>
+          ) : null}
+        </div>
+      );
+    }
+    return arr;
+  }
+
+  if (categories.includes(params.id)) {
+    return <div>{renderCategory()}</div>;
+  } else return <NotFound />;
 }
 
 export default ShowAllProducts;
