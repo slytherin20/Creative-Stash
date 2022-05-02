87b52a252ea8aeeca94624f624dd14c1a96752f9
slytherin20 <sonaligupta2904@gmail.com>
2022-05-02 17:16:33 +0530

    FEAT - Added filterBy price and brand

---
+++ src/components/ProductPage/ProductFilter.jsx
@@ -0,0 +1,118 @@
+import { useState, useRef } from "react";
+
+function ProductFilter({ brands, filterProductsByChoice, resetProducts }) {
+  const [price, setPrice] = useState("-1");
+  const [checked, setChecked] = useState(new Array(brands.length).fill(false));
+  const formRef = useRef(null);
+  function changeRange(e) {
+    setPrice(e.target.value);
+  }
+
+  function submitSelection() {
+    let checkedBrands = [];
+    if (checked.includes(true)) {
+      checkedBrands = getBrandsName();
+    }
+    filterProductsByChoice(price, checkedBrands);
+  }
+
+  function getBrandsName() {
+    let arr = [];
+    for (let i = 0; i < checked.length; i++) {
+      if (checked[i] === true) {
+        arr.push(brands[i]);
+      }
+    }
+    return arr;
+  }
+
+  function checkValue(index) {
+    let temp = checked;
+    temp[index] = !temp[index];
+    setChecked(temp);
+  }
+
+  function resetFilter(e) {
+    e.preventDefault();
+    formRef.current.reset();
+    setPrice("-1");
+    setChecked(new Array(brands.length).fill(false));
+    resetProducts();
+  }
+  return (
+    <div className="w-30 pa2">
+      <h3 className="tc">Filters</h3>
+      <form ref={formRef}>
+        <button type="button" onClick={submitSelection}>
+          Apply
+        </button>
+        <button type="button" onClick={resetFilter}>
+          Clear
+        </button>
+        <div className="price-filter">
+          <p className="b">Price</p>
+          <div className="flex flex-column">
+            <label htmlFor="<500">
+              <input
+                type="radio"
+                name="price"
+                value="500"
+                onChange={changeRange}
+              />
+              Less than ₹500
+            </label>
+            <label htmlFor="500-1000">
+              <input
+                type="radio"
+                name="price"
+                value="1000"
+                onChange={changeRange}
+              />
+              Between ₹500 and ₹1000
+            </label>
+            <label htmlFor="1000-1500">
+              <input
+                type="radio"
+                name="price"
+                value="1500"
+                onChange={changeRange}
+              />
+              Between ₹1000 and ₹1500
+            </label>
+            <label htmlFor="1000-2000">
+              <input
+                type="radio"
+                name="price"
+                value="2000"
+                onChange={changeRange}
+              />
+              Between ₹1500 and ₹2000
+            </label>
+            <label htmlFor="2000+">
+              <input
+                type="radio"
+                name="price"
+                value="200000"
+                onChange={changeRange}
+              />
+              Greater than ₹2000
+            </label>
+          </div>
+        </div>
+        <div className="brands-filter flex flex-column">
+          <p className="b">Brands</p>
+          {brands.map((brand, index) => {
+            return (
+              <label htmlFor={brand} key={brand}>
+                <input type="checkbox" onChange={() => checkValue(index)} />
+                {brand}
+              </label>
+            );
+          })}
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ProductFilter;
--- src/components/ProductPage/ShowProducts.jsx
+++ src/components/ProductPage/ShowProducts.jsx
@@ -1,5 +1,100 @@
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import NotFound from "../NotFound.jsx";
+import ProductFilter from "./ProductFilter.jsx";
+import Item from "../Home Page/Item.jsx";
 function ShowProducts() {
-  return <h1>Hello, this is showProducts page.</h1>;
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [brands, setBrands] = useState([]);
+  const params = useParams();
+  const path = useLocation();
+  let productType = [
+    "Water-Colours",
+    "Oil-Colours",
+    "Spray-Paints",
+    "Acrylics",
+    "Acrylic-Auxilaries",
+    "Gessos-and-Grounds",
+    "Fixative-and-Varnishes",
+    "Oil-Mediums",
+    "Water-Colour-Mediums",
+    "Canvas-Boards",
+    "Canvas-Rolls",
+    "Stretched-Canvas",
+    "Acrylic",
+    "Oil",
+    "Water-Colour",
+    "Acrylic-Markers",
+    "Pigment-Liners",
+    "Twin-Tip-Markers",
+    "Brush-Pens",
+  ];
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    //getting the products
+    let res = await fetch("http://localhost:3000/Products");
+    let data = await res.json();
+    let cat = path.pathname.split("/")[2]; //Getting the second last part of url.
+    cat = cat.split("-").join(" ");
+    let items = data[cat];
+    let subCategory = params.id.split("-").join(" ");
+    setProducts(items[subCategory]);
+    console.table(items[subCategory]);
+    setFilteredProducts(items[subCategory]);
+    //Save brand names.
+    let arr = [];
+    items[subCategory].map((item) => {
+      if (arr.length > 0 && !arr.includes(item.brand)) {
+        arr.push(item.brand);
+      } else if (arr.length === 0) arr.push(item.brand);
+    });
+    setBrands(arr);
+  }
+
+  function filterProductsByChoice(price, checkedBrands) {
+    let items = [];
+    //Filter by price only.
+    if (price < 0 && checkedBrands.length > 0) {
+      items = products.filter((item) => checkedBrands.includes(item.brand));
+    } else if (price > 0 && checkedBrands.length == 0) {
+      //Filter by brands only
+      items = products.filter((item) =>
+        price == 200000
+          ? item.price > 2000
+          : item.price <= price && item.price > price - 500
+      );
+    } else if (price > 0 && checkedBrands.length > 0) {
+      //Filter by both price and brands.
+      items = products.filter(
+        (item) =>
+          (price == 200000
+            ? item.price > 2000
+            : item.price <= price && item.price > price - 500) &&
+          checkedBrands.includes(item.brand)
+      );
+    }
+    setFilteredProducts(items);
+  }
+  function resetProducts() {
+    setFilteredProducts(products);
+  }
+
+  if (!productType.includes(params.id)) return <NotFound />;
+  return (
+    <div className="flex">
+      <ProductFilter
+        brands={brands}
+        filterProductsByChoice={filterProductsByChoice}
+        resetProducts={resetProducts}
+      />
+      <Item items={filteredProducts} title={params.id.split("-").join(" ")} />
+    </div>
+  );
 }
 
 export default ShowProducts;
