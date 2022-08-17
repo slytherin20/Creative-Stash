17b34b214f921f6c90c2ac69662d881eb937b130
slytherin20 <sonaligupta2904@gmail.com>
2022-08-17 14:32:57 +0530

    FIX- Fixed loading sign in case of no order placed

--- src/components/Order/OrderList.jsx
+++ src/components/Order/OrderList.jsx
@@ -4,19 +4,25 @@ import Loading from "../Modals/Loading.jsx";
 import OrderItem from "./OrderItem.jsx";
 function OrderList({ uid, setOrderDetailsHandler }) {
   const [orderList, setOrderList] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => getOrders(), []);
 
   async function getOrders() {
     let orders = await fetchOrderList(uid);
     setOrderList(orders);
+    if (orders.length != 0) setLoading(false);
+    else setLoading(null);
   }
 
   return (
     <div className="w-100 ma2">
       <h2>Orders</h2>
-      {orderList.length === 0 ? (
+      {loading == true ? (
         <Loading />
+      ) : loading == null ? (
+        <div>
+          <p>No orders placed yet</p>
+        </div>
       ) : (
         <article className="w-90">
           {orderList.map((item) => (
