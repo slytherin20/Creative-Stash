f09bcb90f122a82de2457dcf640b780083233416
slytherin20 <sonaligupta2904@gmail.com>
2022-08-23 18:35:06 +0530

    REFACTOR - Banners changed.

--- src/components/Home Page/Highlight.jsx
+++ src/components/Home Page/Highlight.jsx
@@ -1,23 +1,28 @@
-import { useEffect, useState } from "react";
+import PaintBanner from "../../images/banner1.png";
+import WinsorBanner from "../../images/banner2.png";
+import BrustroBanner from "../../images/banner3.png";
+import CanvasBanner from "../../images/banner4.png";
 import Carousel from "./Carousel.jsx";
 
+let images = [
+  {
+    id: 1,
+    original: CanvasBanner,
+  },
+  {
+    id: 2,
+    original: WinsorBanner,
+  },
+  {
+    id: 3,
+    original: PaintBanner,
+  },
+  {
+    id: 4,
+    original: BrustroBanner,
+  },
+];
 function Highlight() {
-  const [images, setImages] = useState();
-
-  useEffect(() => {
-    requestImages();
-  }, []);
-
-  async function requestImages() {
-    let res = await fetch(`${process.env.REACT_APP_MOCKBACKEND}/Carousel`, {
-      headers: {
-        "Transfer-Encoding": "gzip",
-      },
-    });
-    let images = await res.json();
-    setImages(images);
-  }
-
   return (
     <div className="flex justify-center corousel-slides vh-50">
       <Carousel images={images} />
