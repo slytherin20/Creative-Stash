166729235f49921dca6b8991bc2408bc1f7897a8
slytherin20 <sonaligupta2904@gmail.com>
2024-02-17 20:56:41 +0530

    REFACTOR - Removed fetch Image from mock server code

--- src/data/fetchProductImg.js
+++
@@ -1,7 +0,0 @@
-async function fetchProductImg(id) {
-  let res = await fetch(`${process.env.REACT_APP_MOCKBACKEND}/images/${id}`);
-  let img = await res.json();
-  return img.img;
-}
-
-export default fetchProductImg;
